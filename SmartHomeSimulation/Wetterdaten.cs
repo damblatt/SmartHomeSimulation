3603406253587d014da4531bd9111230e9494f5f
damblatt <dam.blatt@gmail.com>
2023-02-13 19:49:00 +0100

    commit

---
+++ SmartHomeSimulation/Wetterdaten.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHomeSimulation
+{
+    internal class Wetterdaten
+    {
+        public bool Regen { get; set; }
+        public double Aussentemperatur { get; set; }
+        public double Windgeschwindigkeit { get; set; }
+
+        public Wetterdaten GetWetterDaten()
+        {
+            return this;
+        }
+    }
+}
---
+++ SmartHomeSimulation/Zimmer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHomeSimulation
+{
+    abstract class Zimmer
+    {
+        protected double temperaturvorgabe;
+        protected bool personImZimmer;
+        public string Name { get; }
+
+        public Zimmer(string name)
+        {
+            this.Name = name;
+        }
+
+        public virtual void VerarbeiteWetterdaten(Wetterdaten wetterdaten)
+        {
+
+        }
+    }
+}
