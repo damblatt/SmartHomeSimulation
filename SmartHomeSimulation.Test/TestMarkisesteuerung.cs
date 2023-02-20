00ef2faf6013a67df3b5c638958a115364c68a27
damblatt <dam.blatt@gmail.com>
2023-02-20 14:40:09 +0100

    test markise fertig

---
+++ SmartHomeSimulation.Test/TestMarkisesteuerung.cs
@@ -0,0 +1,92 @@
+﻿using M320_SmartHome;
+
+namespace SmartHomeSimulation.Test
+{
+    [TestClass]
+    public class TestMarkisesteuerung
+    {
+        [TestMethod]
+        public void TestRegenWind_true()
+        {
+            // Arrange
+            int zeitdauerMinuten = 30;
+
+            var wettersensor = new WettersensorMock(25, 35, true);
+            var wohnung = new Wohnung(wettersensor);
+
+            wohnung.SetTemperaturvorgabe("Wintergarten", 20);
+            wohnung.SetPersonenImZimmer("Wintergarten", false);
+
+            wohnung.GenerateWetterdaten(zeitdauerMinuten);
+
+            // Act
+            var wintergarten = wohnung.GetZimmer<ZimmerMitMarkisensteuerung>("Wintergarten");
+
+            // Assert
+            Assert.AreEqual(wintergarten.MarkiseOffen, true);
+        }
+
+        [TestMethod]
+        public void TestWind_false()
+        {
+            // Arrange
+            int zeitdauerMinuten = 30;
+
+            var wettersensor = new WettersensorMock(25, 35, false);
+            var wohnung = new Wohnung(wettersensor);
+
+            wohnung.SetTemperaturvorgabe("Wintergarten", 20);
+            wohnung.SetPersonenImZimmer("Wintergarten", false);
+
+            wohnung.GenerateWetterdaten(zeitdauerMinuten);
+
+            // Act
+            var wintergarten = wohnung.GetZimmer<ZimmerMitMarkisensteuerung>("Wintergarten");
+
+            // Assert
+            Assert.AreEqual(wintergarten.MarkiseOffen, false);
+        }
+
+        [TestMethod]
+        public void TestHoehereAussentemp_false()
+        {
+            // Arrange
+            int zeitdauerMinuten = 30;
+
+            var wettersensor = new WettersensorMock(25, 20, false);
+            var wohnung = new Wohnung(wettersensor);
+
+            wohnung.SetTemperaturvorgabe("Wintergarten", 20);
+            wohnung.SetPersonenImZimmer("Wintergarten", false);
+
+            wohnung.GenerateWetterdaten(zeitdauerMinuten);
+
+            // Act
+            var wintergarten = wohnung.GetZimmer<ZimmerMitMarkisensteuerung>("Wintergarten");
+
+            // Assert
+            Assert.AreEqual(wintergarten.MarkiseOffen, false);
+        }
+
+        [TestMethod]
+        public void TestHoehereTempKeinWindKeinRegen_true()
+        {
+            // Arrange
+            int zeitdauerMinuten = 30;
+
+            var wettersensor = new WettersensorMock(15, 35, false);
+            var wohnung = new Wohnung(wettersensor);
+
+            wohnung.SetTemperaturvorgabe("Wintergarten", 20);
+            wohnung.SetPersonenImZimmer("Wintergarten", false);
+
+            wohnung.GenerateWetterdaten(zeitdauerMinuten);
+
+            // Act
+            var wintergarten = wohnung.GetZimmer<ZimmerMitMarkisensteuerung>("Wintergarten");
+
+            // Assert
+            Assert.AreEqual(wintergarten.MarkiseOffen, true);
+        }
+    }
+}
