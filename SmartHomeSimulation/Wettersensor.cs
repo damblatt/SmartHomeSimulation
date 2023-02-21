c3924d89e934bb086010c84c2a4e19fb48675316
damblatt <dam.blatt@gmail.com>
2023-02-21 14:29:47 +0100

    xml für die klasse wettersensor

--- SmartHomeSimulation/Wettersensor.cs
+++ SmartHomeSimulation/Wettersensor.cs
@@ -1,15 +1,36 @@
 ﻿namespace M320_SmartHome {
+    /// <summary>
+    /// Wettersensor implementiert das Interface Wettersensor.
+    /// </summary>
     public class Wettersensor : IWettersensor
     {
         private Random random;
+        /// <summary>
+        /// saves the value of the current temperature
+        /// </summary>
         private double currentTemp;
+        /// <summary>
+        /// sets the maximum temperature
+        /// </summary>
         private const int MAX_TEMP = 35;
+        /// <summary>
+        /// sets the maximum temperature
+        /// </summary>
         private const int MIN_TEMP = -25;
+        /// <summary>
+        /// standard constructor, welcher die temperatur bestimmt
+        /// </summary>
         public Wettersensor() {
             this.random = new Random(Guid.NewGuid().GetHashCode());
             this.currentTemp = random.Next(MIN_TEMP, MAX_TEMP);
         }
 
+        /// <summary>
+        /// Wetterdaten werden generiert
+        /// </summary>
+        /// <returns>
+        /// Objekt der Klasse Wetterdaten mit Aussentemperatur, Windgeschwindigkeit und Regen
+        /// </returns>
         public Wetterdaten GetWetterdaten() {
             var rand = this.random.NextDouble();
 
